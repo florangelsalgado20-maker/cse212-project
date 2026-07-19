97e26c649337f8d0b478119276ccf23ba14c75ff
Flor <florangelsalgado20@gmail.com>
2026-07-19 18:36:44 -0400

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Node
 {
     public int Data { get; set; }
@@ -11,11 +13,13 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
+        // Problema 1: Prevenir la inserción de valores duplicados
+        if (value == Data)
         {
-            // Insert to the left
+            return;
+        }
+        else if (value < Data)
+        {
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +27,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +36,29 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
+        // Problema 2: Verificar si el valor existe en el árbol
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data && Left is not null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right is not null)
+        {
+            return Right.Contains(value);
+        }
+        
         return false;
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Problema 4: Calcular la altura del árbol de forma recursiva
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
